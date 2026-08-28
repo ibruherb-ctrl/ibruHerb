f6d7032e07efb935bc33a19d4220afe9162a2120
ibruherb-ctrl <ibruherb@gmail.com>
2026-08-28 15:07:54 +0530

    Update products (0 items)

--- products.js
+++ products.js
@@ -1,11 +1,1 @@
-export const products = [
-  {
-    "id": 1,
-    "name": "Test 2.0",
-    "price": 888,
-    "stock": 20,
-    "category": "Tech",
-    "image": "https://static.vecteezy.com/system/resources/thumbnails/056/654/427/small/mockup-showcases-collection-of-tech-gadgets-designed-in-isometric-view-featuring-stylish-headphones-sleek-power-bank-and-modern-smartphone-all-ready-for-branding-and-customization-photo.jpeg",
-    "description": "This is new version"
-  }
-];
+export const products = [];
